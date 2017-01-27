fe7ccacd16839e19a2a9d3832994abf53fa78717
Cong <809556311@qq.com>
2017-01-28 01:58:21 +0800

    title

--- pages/index/index.js
+++ pages/index/index.js
@@ -4,11 +4,20 @@ var app = getApp()
 Page({
   data: {
     app_name: '美食地图'
-    
+    ,points:[
+      {
+        name: '阿婆牛杂'
+      }
+      ,{
+        name: '陈伯拉肠'
+      }
+      ,{
+        name: '湿炒牛河'
+      }
+    ]
   },
   //跳转到地图
   showMap: function() {
-    
     wx.navigateTo({
       url: '../map/map'
     })
