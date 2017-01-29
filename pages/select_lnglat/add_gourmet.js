c44a34c75c5e6eaae3465fc45ae9735075a9ccc6
Cong <809556311@qq.com>
2017-01-30 01:17:46 +0800

    upload pics ok

--- pages/select_lnglat/add_gourmet.js
+++ pages/select_lnglat/add_gourmet.js
@@ -1,5 +1,7 @@
 var Bmob = require('../../utils/bmob.js');
 
+var urls = [];
+
 module.exports = {
   editPos: function(){
       console.log('editPos')
@@ -10,6 +12,10 @@ module.exports = {
   }
   //add pictures
   ,add_pics:function(){
+    if(urls.length == 9){
+      alert("最多添加9张图片")
+      return;
+    }
     var that = this;
       wx.chooseImage({
         count: 9, // 默认9
@@ -21,29 +27,39 @@ module.exports = {
           console.log('tempFilePaths', tempFilePaths);
           //
           var count = 0;
-          var urls = [];
+          var beishu = 100/tempFilePaths.length;
           if (tempFilePaths.length > 0) {
+
+              // var name="1.jpg";//上传的图片的别名
+              // var file=new Bmob.File(name,tempFilePaths);
+              // file.save().then(function(res){
+              //   console.log('upload res',res);
+              // },function(error){
+              //   console.log(error);
+              // })
+            
             for(var i = 0; i< tempFilePaths.length; i++){
                 var name = i+".jpg";//上传的图片的别名
-                var file = new Bmob.File(name, tempFilePaths[i]);
+                var file = new Bmob.File(name, [tempFilePaths[i]]);
                 console.log(name,tempFilePaths[i])
                 file.save().then(function (res) {
                   console.log('upload',res.url());
                   urls.push(res.url());
                   count++;
-                  updatePrgress(count);
+                  updatePrgress(count, beishu);
                 }, function (error) {
                   console.log('upload',error);
                   count++;
-                  updatePrgress(count);
+                  updatePrgress(count, beishu);
                 })
             }
+
           }
 
-          function updatePrgress(count){
+          function updatePrgress(count, beishu){
               that.setData({
                 pics_number: count
-                ,upload_progress: (count+1) * 11.11111111
+                ,upload_progress: (count+1) * beishu
                 ,urls: urls
               })
           }
