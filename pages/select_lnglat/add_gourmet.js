70777ec9e19c19fcec108dea963cf7d1ba7526bb
Cong <809556311@qq.com>
2017-01-31 07:32:34 +0800

    改用微信的选择位置

--- pages/select_lnglat/add_gourmet.js
+++ pages/select_lnglat/add_gourmet.js
@@ -3,6 +3,7 @@ var Bmob = require('../../utils/bmob.js');
 var utils = require('../../utils/util.js');
 var CS = require('../../utils/CS.js');
 
+var gourmet_address = "";
 var gourmet_title = "";
 var gourmet_desc = "";
 var urls = [];
@@ -20,18 +21,8 @@ function clearData(){
 }
 
 module.exports = {
-  editPos: function(){
-      console.log('editPos');
-      this.getLngLat(value=>{
-        geopoint = value;
-        //
-        this.setData({
-          //隐藏地图
-          "hide": true
-          //显示输入框
-          ,"show": true
-        })
-      });   
+  onReady: function() {
+    this.chooseLocation()
   }
   //add pictures
   ,add_pics:function(){
@@ -50,14 +41,7 @@ module.exports = {
           console.log('tempFilePaths', tempFilePaths);
           //
           if (tempFilePaths.length > 0) {
-
-              // var name="1.jpg";//上传的图片的别名
-              // var file=new Bmob.File(name,tempFilePaths);
-              // file.save().then(function(res){
-              //   console.log('upload res',res);
-              // },function(error){
-              //   console.log(error);
-              // })
+              
             for(var i = 0; i< tempFilePaths.length; i++){
                 utils.showLoading("上传中");
                 console.log("uploading...")
@@ -79,20 +63,18 @@ module.exports = {
           function updatePrgress(){
               headurl = urls.length > 0 ? urls[0] : "";
               that.setData({
-                pics_number: urls.length
-                ,urls: urls
+                urls: urls
                 ,headurl: headurl
                 ,show_headurl: headurl == "" ? false : true
               })
+              utils.showSuccess("请稍等一下就加载出来");
               setTimeout(function(){
                 utils.hideLoading();
-                utils.showSuccess("请稍等一下就加载出来");
               },2000)
           }
         }
       })
   }
-
   //preview imgs
   ,preview: function(){
     if(urls.length == 0) return;
@@ -101,7 +83,6 @@ module.exports = {
       urls: urls // 需要预览的图片http链接列表
     })
   }
-
   //切换首页图图
   ,checkout: function(){
     headurlIndex++;
@@ -111,21 +92,38 @@ module.exports = {
       headurl: headurl
     })
   }
-
-  //加载至少一张图片OK
-  ,loadPicOk: function(e){
-      this.setData({
-          //显示输入框
-          "show_submit": true
+  ,chooseLocation: function(){
+    var that = this;
+    wx.chooseLocation({
+      success: function(ret){
+        console.log('chooseLocation',ret)
+        gourmet_address = ret.address;
+        gourmet_title = ret.name;
+        that.setData({
+          address: gourmet_address
+          ,title: gourmet_title
         })
+        geopoint = {
+          latitude: +ret.latitude //数值
+          ,longitude: +ret.longitude //数值
+        }
+      }
+      ,cancel: function(){
+        geopoint = null;//退出之后对象清空
+      }
+    })
   }
 
   //
-  ,confirmTitle: function(e){
+  ,inputAddress: function(e){
+      console.log(e.detail);
+      gourmet_address = e.detail.value
+  }
+  ,inputTitle: function(e){
       console.log(e.detail);
       gourmet_title = e.detail.value
   }
-  ,confirmDesc: function(e){
+  ,inputDesc: function(e){
       console.log(e.detail);
        gourmet_desc = e.detail.value
   }
@@ -140,11 +138,17 @@ module.exports = {
       console.log('gourmet_title',gourmet_title);
       console.log('gourmet_desc',gourmet_desc);
       // 检查参数
+      if(!geopoint){
+          return utils.showModal('错误','请选择位置')
+      }
       if(urls.length == 0){
           return utils.showModal('错误','至少上传一张图片')
       }
-      if(gourmet_title === ""){
+      if(gourmet_title.trim() === ""){
           return utils.showModal('错误','请输入美食点名称')
+      }
+      if(gourmet_address.trim() === ""){
+          return utils.showModal('错误','地址不能为空哦')
       }
 
       app.getUserInfo(userinfo=>{
@@ -154,12 +158,13 @@ module.exports = {
         gourmet.set("user_nickname", userinfo.nickName);
         gourmet.set("description", gourmet_desc);
         gourmet.set("head_url", headurl);
-        // var location = geopoint.longitude+','+geopoint.latitude;
-        var location = new Bmob.GeoPoint({latitude: geopoint.latitude, longitude: geopoint.longitude});
+        var location = new Bmob.GeoPoint(geopoint);
+        // var point = new Bmob.GeoPoint({latitude: 23.345644, longitude: 112.234454});
         gourmet.set("location", location);
         gourmet.set("openid", userinfo.openid);
         gourmet.set("urls", urls);
         gourmet.set("title", gourmet_title);
+        gourmet.set("address", gourmet_address);
         gourmet.set("user_avatar", userinfo.avatarUrl);
         //添加数据，第一个入口参数是null
         gourmet.save(null, {
--- pages/select_lnglat/select_lnglat.js
+++ pages/select_lnglat/select_lnglat.js
@@ -7,32 +7,11 @@ var combinePage = {
     data:{
       map_width: 380
       ,map_height: 380
-      //已添加的图片个数
-      ,pics_number: 0
-      ,upload_progress: 0
       ,urls:[]
     }
     //show current position
     ,onLoad: function(){
     var that = this;
-    // 获取定位，并把位置标示出来
-    app.getLocationInfo(function(locationInfo){
-        console.log('map',locationInfo);
-        that.setData({
-          longitude: locationInfo.longitude
-          ,latitude: locationInfo.latitude
-          ,markers:[
-            {
-            id: 0
-            ,iconPath: "../../imgs/ic_position.png"
-            ,longitude: locationInfo.longitude
-            ,latitude: locationInfo.latitude
-            ,width: 30
-            ,height: 30
-            }
-          ]
-        })
-    })
 
     //set the width and height
     // 动态设置map的宽和高
@@ -44,61 +23,9 @@ var combinePage = {
           //设置预览小图的大小
           ,img_width: width/3 - 25
           ,img_height: width/3 -25
-          ,controls: [{
-            id: 1,
-            iconPath: '../../imgs/ic_location.png',
-            position: {
-              left: width/2 - 8,
-              top: width/2 - 16,
-              width: 30,
-              height: 30
-            },
-            clickable: true
-          }]
         })
     })
-
-  }
-  //获取中间点的经纬度，并mark出来
-  ,getLngLat: function(cb){
-      console.log('getlngLat');
-      var that = this;
-      this.mapCtx = wx.createMapContext("map4select");
-      this.mapCtx.getCenterLocation({
-        success: function(res){
-
-            var geopoint = {
-              longitude: res.longitude
-              ,latitude: res.latitude
-            }
-
-            if(cb){cb(geopoint)}
-
-            that.setData({
-            longitude: geopoint.longitude
-            ,latitude: geopoint.latitude
-            ,markers:[
-              {
-              id: 0
-              ,iconPath: "../../imgs/ic_position.png"
-              ,longitude: geopoint.longitude
-              ,latitude: geopoint.latitude
-              ,width: 30
-              ,height: 30
-              }
-            ]
-          })
-
-        }
-      })
-  }
-  ,regionchange(e) {
-    // 地图发生变化的时候，获取中间点，也就是用户选择的位置
-    if(e.type == 'end'){
-        this.getLngLat(geopoit=>{
-          console.log(geopoit)
-        })
-    }
+    
   }
 }
 
